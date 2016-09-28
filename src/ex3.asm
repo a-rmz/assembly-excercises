5bdf44a37ec0227aec0af3ced95ea69e2fa32a18
a-rmz <armzprz@gmail.com>
2016-09-28 11:36:55 -0500

    Working Ex3

--- src/ex3.asm
+++ src/ex3.asm
@@ -1,2 +1,15 @@
 ; 3) Hacer un programa para que el acumulador
-; pase de 0 a 10 se uno en uno y luego se repita el proceso
+; pase de 0 a 10 de uno en uno y luego se repita el proceso
+
+            ORG 00H
+            JMP MAIN
+
+            ORG 40H
+MAIN:       MOV A, #00H
+            MOV R0, #0AH
+
+CYCLE:      INC A
+            DJNZ R0, CYCLE
+            JMP MAIN
+
+            END
