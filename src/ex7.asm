b340fec50e6cd6054d355e4e34612c1046d42c38
a-rmz <armzprz@gmail.com>
2016-09-30 11:46:09 -0500

    Ex7 solved

--- src/ex7.asm
+++ src/ex7.asm
@@ -5,3 +5,21 @@
 ; menos significativos en los 4 bits menos
 ; significativos del registro R2. Los 4 bits más
 ; significativos de R1 y R2, deben quedar en cero.
+
+            ORG 00H
+            JMP MAIN
+
+            ORG 40H
+MAIN:       MOV R0, #10100101B  ; -> DEBUG PURPOSES ONLY
+
+            MOV A, R0           ; LOAD A WITH THE VALUE
+            SWAP A              ; SWAP THE NIBBLES
+            ANL A, #00001111B   ; AND THE LOWER NIBBLE
+            MOV R1, A           ; MOVE IT TO R1
+
+            MOV A, R0           ; LOAD A WITH THE VALUE
+                                ; NO NEED TO SWAP THIS TIME
+            ANL A, #00001111B   ; AND THE LOWER NIBBLE
+            MOV R2, A
+
+            END
